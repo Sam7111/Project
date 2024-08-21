d4f92421f453b7488dc3e69ab155a52d8805c704
sammed <sammedpatil527@gmail.com>
2024-08-21 11:37:07 +0530

    Updated code command in codegit file

---
+++ Codegit.js
@@ -0,0 +1,7 @@
+// create Respository in github.
+// git config --global user.name "Your Name"
+// git config --global user.email "your.email@example.com"
+// git init
+// git remote add origin https://github.com/yourusername/your-repo-name.git
+// git clone https://github.com/username/repository.git
+
